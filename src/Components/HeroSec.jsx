16c3ecaa25e5028758fe3078a9e6649a6c7b2901
Crafty23Jay <alasatisaheedjamal@gmail.com>
2025-06-26 21:17:24 +0100

    home page convertion

---
+++ src/Components/HeroSec.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../Pages/Services.css'
+
+const HeroSec = () => {
+  return (
+    <main class="hero">
+            <div class="hero-cont">
+                <h1>Great Shipping Services with Logistical Support & Technical Services</h1>
+                <p>Bishu Engineering puts together a rich array of Services supported by Certified Staff and 
+                    useful partnerships
+                </p>
+            </div>
+        </main>
+  )
+}
+
+export default HeroSec
